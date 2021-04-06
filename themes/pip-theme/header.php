c0f7d3589b4586ae28195e2b4c6a11daf24a194e
pattyok <patty.ohara@gmail.com>
2021-04-06 08:54:33 -0700

    adding gq

--- themes/pip-theme/header.php
+++ themes/pip-theme/header.php
@@ -26,6 +26,15 @@ namespace WP_Rig\WP_Rig;
 		<?php
 	}
 	?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-XESTC2330H"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-XESTC2330H');
+	</script>
 
 	<?php wp_head(); ?>
 </head>
