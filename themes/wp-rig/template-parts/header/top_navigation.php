0f1591cdeb07d22b6ebf8dd24e7b27abc1f0b802
pattyok <patty.ohara@gmail.com>
2022-03-03 16:51:27 -0800

    prep for gtranslate

--- themes/wp-rig/template-parts/header/top_navigation.php
+++ themes/wp-rig/template-parts/header/top_navigation.php
@@ -14,6 +14,15 @@ if ( ! wp_rig()->is_secondary_nav_menu_active() ) {
 ?>
 
 <nav id="top-navigation" class="top-navigation " aria-label="<?php esc_attr_e( 'Top menu', 'wp-rig' ); ?>">
+<div class="header-top">
+
+<?php if ( is_active_sidebar( 'header-top' ) ) { ?>
+
+				<?php dynamic_sidebar( 'header-top' ); ?>
+
+
+	<?php } ?>
+</div>
 	<div class="top-menu-container">
 		<?php wp_rig()->display_secondary_nav_menu( array( 'menu_id' => 'secondary' ) ); ?>
 	</div>
